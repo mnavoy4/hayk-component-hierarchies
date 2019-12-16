c94b8ff1405cc5ab98fbb43fb0b4821011b443c6
Kyle Coberly <kyle.coberly@gmail.com>
2019-12-16 16:44:59 -0700

    Adding starter code

---
+++ google-search-results/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import data from './data'
+
+export default class App {
+    state = data
+    render(){
+        return (
+            <div className="App">
+            </div>
+        )
+    }
+}
+
+export default App
